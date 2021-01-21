86ecaecbcb47fbfc4a7de87f024fd4f83fb764f4
carmelo343 <carmelo.giuliano1@gmail.com>
2021-01-21 22:31:06 +1100

    Find and replace all email variables

--- src/company-tax.js
+++ src/company-tax.js
@@ -2,6 +2,9 @@ const electron = require('electron');
 const path = require('path');
 const replace = require('replace-in-file');
 
+const filePath = process.env.PWD + '/email-templates/company-tax.txt';
+
+//#region form data
 clientFirstName = document.getElementById("clientFirstName");
 clientLastName = document.getElementById("clientLastName");
 companyName = document.getElementById("companyName");
@@ -11,22 +14,17 @@ amountPayable = document.getElementById("amountPayable");
 amountRefundable = document.getElementById("amountRefundable");
 additionalInfo = document.getElementById("additionalInfo");
 btnSubmit = document.getElementById("btnSubmit");
+//#endregion
+
+
 
 btnSubmit.addEventListener("click", function () {
-  //alert(clientFirstName.value);
-
-  var basePath = process.env.PWD
-  var filePath = basePath + '/email-templates/company-tax.txt'
-
   const options = {
     files: filePath,
-    from: /{{companyName}}/g,
-    to: companyName.value,
+    from: [/{{clientFirstName}}/g, /{{clientLastName}}/g, /{{companyName}}/g, /{{companyEntityType}}/g, /{{financialYear}}/g, /{{amountPayable}}/g, /{{amountRefundable}}/g, /{{additionalInfo}}/g],
+    to: [clientFirstName.value, clientLastName.value, companyName.value, companyEntityType.value, financialYear.value, amountPayable.value, amountRefundable.value, additionalInfo.value]
   };
 
   const results = replace(options);
 
 });
-
-
-
