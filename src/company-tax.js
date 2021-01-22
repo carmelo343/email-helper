24b5c178d59df118a1d2f0846bd85b1e3ffe9a69
carmelo343 <carmelo.giuliano1@gmail.com>
2021-01-23 02:19:31 +1100

    Generate and open .eml in email client

--- src/company-tax.js
+++ src/company-tax.js
@@ -3,19 +3,26 @@ const electron = require('electron');
 const { shell } = require('electron');
 const { get } = require('http');
 const path = require('path');
-const replace = require('replace-in-file');
-var emlformat = require('eml-format');
+//const replace = require('replace-in-file');
 const { render, renderFile } = require('template-file');
 const fs = require('fs');
 const emlTemplatePath = process.env.PWD + '/eml-templates/';
+
+let emlSkeleton = "Subject: {{subject}}\nX-Unsent: 1\nContent-Type: text/html\n\n{{body}}"
 
 $(function () {
 
   $('#btnSubmit').on('click', function () {
     let formData = getFormData();
     let email = JSON.parse(fs.readFileSync(emlTemplatePath + 'company-tax.json'));
-    let str = render(email.subject, formData);
-    alert(str);
+
+    email.subject = render(email.subject, formData);
+    email.body = render(email.body, formData);
+
+    let emlStr = render(emlSkeleton, email);
+    fs.writeFileSync(emlTemplatePath + 'company-tax.eml', emlStr);
+    shell.openPath(emlTemplatePath + 'company-tax.eml');
+
   });
 
 });
