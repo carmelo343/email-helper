47a5d160ec1f65ca54b2cb7f2e29677566f9893d
carmelo343 <carmelo.giuliano1@gmail.com>
2021-02-08 12:52:09 +1100

    Payroll tax add/delete items from table

--- renderer/payroll-tax.js
+++ renderer/payroll-tax.js
@@ -3,14 +3,23 @@ const $ = jQuery = require('jquery');
 const app = require('./app');
 
 
-
+let itemCount = 0;
+let taxItems = [];
 $('#addTaxItem').on('click', function (event) {
   event.preventDefault();
+  itemCount++;
 
   let state = $('#state').val();
   let taxAmount = $('#taxAmount').val();
   let taxType = $('input[name="taxType"]:checked').val();
   let paymentType = $('input[name="paymentType"]:checked').val();
+
+  taxItems.push({
+    state: state,
+    taxAmount: taxAmount,
+    taxType: taxType,
+    paymentType: paymentType
+  });
 
   $('#taxItemTable').find('tbody')
     .append('<tr>'
@@ -18,11 +27,18 @@ $('#addTaxItem').on('click', function (event) {
       + `<td>${taxAmount}</td>`
       + `<td>${taxType}</td>`
       + `<td>${paymentType}</td>`
-      + '<td><button id="deleteItemBtn" class="btn btn-danger" formnovalidate>Delete</button></td>');
+      + `<td><input type="button" id="deleteItemBtn${itemCount}" value="Delete" class="btn btn-danger"></button></td>`);
+
+  $('[id^="deleteItemBtn"]').on('click', function (event) {
+    event.preventDefault();
+    let index = $(this).closest('tr').index();
+    if (index < 1) {
+      return;
+    }
+    $(this).closest('tr').remove();
+    taxItems.splice(index - 1, 1);
+  });
 
 });
 
-$('#deleteItemBtn').on('click', function (event) {
-  event.preventDefault();
-  debugger;
-});
+
